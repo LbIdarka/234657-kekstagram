4014b19583e32913f017e6336da1cd9109f0e1d0
Alena Nikolaenko <lbidarka@gmail.com>
2018-06-08 23:57:12 +0300

    создает массив из генерируемых объектов

--- js/pictures.js
+++ js/pictures.js
@@ -5,8 +5,7 @@
   var LIKES_MAX = 200;
   var COMMENTS =['Всё отлично!', 'В целом всё неплохо. Но не всё.',   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
   var DESCRIPTION = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
-
-  var fotos = [];
+  var FOTOS_NUMBERS = 25;
 
   var getRandomLikes = function (min, max) {
     return Math.floor(Math.random() * (max - min)) + Math.floor(min);
@@ -24,11 +23,18 @@
     return getRandomString(comments);
   };
 
-  foto = {
-    url: '../photos/1.jpg',
-    likes: getRandomLikes(LIKES_MIN, LIKES_MAX),
-    comments: getComments,
-    description: getRandomString(DESCRIPTION)
-  };
+  var getFotos = function () {
+    var fotos = [];
 
+    for (var i = 0; i < FOTOS_NUMBERS; i++) {
+      var foto = {
+        url: '../photos/' + (i + 1) + '.jpg',
+        likes: getRandomLikes(LIKES_MIN, LIKES_MAX),
+        comments: getComments(),
+        description: getRandomString(DESCRIPTION)
+      };
+      fotos.push(foto);
+    }
+    return fotos;
+  };
 })();
