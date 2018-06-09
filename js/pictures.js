e6c5e447b017c6b1e208655ef69bf0bc47f8acf9
Alena Nikolaenko <lbidarka@gmail.com>
2018-06-09 21:46:13 +0300

    создает DOM элементы, заполняет их данными из массива и отрисовывает

--- js/pictures.js
+++ js/pictures.js
@@ -6,6 +6,8 @@
   var COMMENTS =['Всё отлично!', 'В целом всё неплохо. Но не всё.',   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
   var DESCRIPTION = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
   var FOTOS_NUMBERS = 25;
+  var similarFotoTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
+  var similarFotoList = document.querySelector('.pictures');
 
   var getRandomLikes = function (min, max) {
     return Math.floor(Math.random() * (max - min)) + Math.floor(min);
@@ -28,7 +30,7 @@
 
     for (var i = 0; i < FOTOS_NUMBERS; i++) {
       var foto = {
-        url: '../photos/' + (i + 1) + '.jpg',
+        url: 'photos/' + (i + 1) + '.jpg',
         likes: getRandomLikes(LIKES_MIN, LIKES_MAX),
         comments: getComments(),
         description: getRandomString(DESCRIPTION)
@@ -37,4 +39,24 @@
     }
     return fotos;
   };
+
+  var renderFoto = function (foto) {
+    var fotoElement = similarFotoTemplate.cloneNode(true);
+
+    fotoElement.querySelector('.picture__img').src = foto.url;
+    fotoElement.querySelector('.picture__stat--likes').textContent = foto.likes;
+    fotoElement.querySelector('.picture__stat--comments').textContent = foto.comments;
+
+    return fotoElement;
+  };
+
+  var addFoto = function () {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < getFotos().length; i++) {
+      fragment.appendChild(renderFoto(getFotos()[i]));
+    }
+    return similarFotoList.appendChild(fragment);
+  };
+  addFoto();
 })();
